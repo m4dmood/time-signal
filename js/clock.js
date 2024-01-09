171483d1bbc680321351d3dd347e2599e0c4931c
m4dmood <56325229+m4dmood@users.noreply.github.com>
2024-01-09 16:44:33 +0100

    analog clock implementation

--- js/clock.js
+++ js/clock.js
@@ -7,7 +7,13 @@ window.onload = (e) => {
 setInterval(() => {
     current = new Date();
     let hh = current.getHours() >= 10 ? current.getHours() : '0' + current.getHours();
+    document.getElementById("hr").style.transform = `rotate( ${(hh * 360) / 12}deg )`;
+
     let mm = current.getMinutes() >= 10 ? current.getMinutes() : '0' + current.getMinutes();
+    document.getElementById("min").style.transform = `rotate( ${(mm * 360) / 60}deg )`;
+
     let ss = current.getSeconds() >= 10 ? current.getSeconds() : '0' + current.getSeconds();
+    document.getElementById("sec").style.transform = `rotate( ${(ss * 360) / 60}deg )`;
+    
     document.getElementById("digital-clock").innerHTML = `${hh}:${mm}:${ss}`;
 }, 1000);
